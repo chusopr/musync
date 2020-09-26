5959649aa597c2a125bc13b04f3a1cafa8910976
Jesús Pérez Rey (Chuso) <git@chuso.net>
2020-09-26 22:57:22 +0200

    Amazon: replace tgransparent sign-in with a browser window instead. Maintaining Amazon sign-in scraping causes too much work, in unstable and less secure.

--- modules/amazon/__init__.py
+++ modules/amazon/__init__.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QGridLayout, QLabel, QDial
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.common.exceptions import WebDriverException, TimeoutException
 from urllib.parse import urlparse
 import requests, modules, json
 from appdirs import user_cache_dir
@@ -84,6 +84,11 @@ class OtpForm(QDialog):
     def get_response(self):
         return self.__otp_response
 
+class amzn_object_exists(object):
+    def __call__(self, driver):
+        return driver.execute_script('return typeof amznMusic !== "undefined" && "appConfig" in amznMusic && "customerId" in amznMusic.appConfig;')
+
+
 class SourceModule(modules.SourceModule):
     __id = "amazon"
     __name = "Amazon Music"
@@ -95,7 +100,7 @@ class SourceModule(modules.SourceModule):
 
     __login_url = "https://www.amazon.com/gp/dmusic/cloudplayer/forceSignIn/"
     __domain = "music.amazon.com"
-    # TODO: Using Chromedriver for debugging, will be replaced by PhantomJS
+    # TODO: Support other browsers
     __chromedriver_path = "/usr/bin/chromedriver"
 
     def __set_session_file(self):
@@ -176,147 +181,42 @@ class SourceModule(modules.SourceModule):
 
         return r
 
-    def __submit_login(self, username, password, redirected=False):
-        userInput = self.__webdriver.find_element_by_id('ap_email')
-        passInput = self.__webdriver.find_element_by_id('ap_password')
-        if not userInput or not passInput:
-            self.__possibly_outdated("Username and/or password input boxes not found in %s site." % self.__name, self)
-            return None
-        userInput.clear()
-        passInput.clear()
-        userInput.send_keys(username)
-        passInput.send_keys(password)
+    def __track_metadata(self, d):
+        track = {}
+        track["disc"]     = d["discNum"]      if "discNum"      in d else ""
+        track["track"]    = d["trackNum"]     if "trackNum"     in d else ""
+        track["artist"]   = d["artistName"]   if "artistName"   in d and d["artistName"] != "Unknown Artist" else ""
+        track["title"]    = d["title"]        if "title"        in d else ""
+        track["duration"] = d["duration"]     if "duration"     in d else ""
+        track["album"]    = d["albumName"]    if "albumName"    in d and d["albumName"] != "Unknown Album" else ""
+        track["genre"]    = d["primaryGenre"] if "primaryGenre" in d and d["primaryGenre"] != "Unknown Genre" else ""
+        return track
 
-        captcha = False
-        try:
-            captcha = self.__webdriver.find_element_by_id('image-captcha-section')
-        except NoSuchElementException:
-            pass
+    def authenticate(self, window, force=False):
 
-        if captcha:
-            captcha_img = captcha.find_element_by_tag_name("img")
-            captchaForm = CaptchaForm(captcha_img.get_attribute("src"))
-            captchaForm.exec()
-            if not captchaForm.get_response():
-                return False
-            captcha_input = self.__webdriver.find_element_by_name("guess")
-            captcha_input.clear()
-            captcha_input.send_keys(captchaForm.get_response())
-            # A CAPTCHA resets the redirect detector
-            redirected = False
+        if self.__authenticated and not force:
+            return True
 
-        try:
-            rememberMe_checkbox = self.__webdriver.find_element_by_name("rememberMe")
-            if not rememberMe_checkbox.is_selected():
-                rememberMe_checkbox.click()
-        except NoSuchElementException:
-            pass
-        passInput.submit()
-
-        try:
-            self.__webdriver.find_element_by_id('auth-error-message-box')
-            # TODO return error message by Amazon
-            errorMsg = QMessageBox(QMessageBox.Warning, "Invalid credentials", "The provided credentials%s are not valid for %s.\nPlease make sure the provided e-mail address and password are correct." % (" or text for the CAPTCHA image" if captcha else "", self.__name), QMessageBox.Ok)
-            errorMsg.setModal(True)
-            errorMsg.exec()
-            return False
-        except NoSuchElementException:
-            pass
-
-        otp_error = ""
-        try:
-            otp = self.__webdriver.find_element_by_id('auth-mfa-otpcode')
-        except NoSuchElementException:
-            pass
-
-        while otp:
-            # Show OTP query form
-            otpForm = OtpForm(otp_error)
-            otpForm.exec()
-            # User aborted? Finish
-            if not otpForm.get_response():
-                return False
-            # Input OTP details
-            otp.clear()
-            otp.send_keys(otpForm.get_response())
-            # Try to check the box to not ask for OTP again
-            # Ignore it if it fails
-            try:
-                otpRemember_checkbox = self.__webdriver.find_element_by_id("auth-mfa-remember-device")
-                if not otpRemember_checkbox.is_selected():
-                    otpRemember_checkbox.click()
-            except NoSuchElementException:
-                pass
-
-            # Submit OTP form
-            otp.submit()
-            # Check if we are asked for the OTP again
-            # meaning the provided one is not valid
-            otp = False
-            try:
-                otp = self.__webdriver.find_element_by_id('auth-mfa-otpcode')
-            except NoSuchElementException:
-                pass
-            # If we are asked again for the OTP, try to get the error message
-            # to return it to the user
-            if otp:
-                try:
-                    otp_error_box = self.__webdriver.find_element_by_id('auth-error-message-box')
-                    otp_error = otp_error_box.find_element_by_tag_name("span").text
-                except NoSuchElementException:
-                    pass
-
-        # We only test for a redirect once
-        if not redirected:
-            redirect_found = False
-            try:
-                self.__webdriver.find_element_by_id('ap_email')
-                self.__webdriver.find_element_by_id('ap_password')
-                redirect_found = True
-            except NoSuchElementException:
-                pass
-
-            if redirect_found:
-                return self.__submit_login(username, password, redirected=True)
-
-        return True
-
-    def __login(self, authDialog, username, password):
-        waitMsg = QMessageBox(QMessageBox.Information, "Authenticating...", "Please wait while you are being authenticated with %s." % self.__name, QMessageBox.NoButton, authDialog)
-        waitMsg.setModal(True)
-        waitMsg.show()
         if self.__webdriver == None:
             self.__webdriver = webdriver.Chrome(executable_path=self.__chromedriver_path)
         self.__webdriver.get(self.__login_url)
-        login_result = self.__submit_login(username, password)
 
-        if login_result == None:
-            waitMsg.close()
-            if self.__webdriver is not None:
-                self.__webdriver.quit()
-                self.__webdriver = None
-            self.__possibly_outdated("It was not possible to authenticate to %s." % self.__name, authDialog)
-            authDialog.reject()
-            return login_result
-        elif login_result == False:
-            waitMsg.close()
-            return login_result
+        element = False
+        while not element:
+            try:
+                wait = webdriver.support.ui.WebDriverWait(self.__webdriver, 3)
+                element = wait.until(amzn_object_exists())
+            except TimeoutException:
+                pass
+            except WebDriverException as e:
+                self.__main.statusBar().showMessage(e)
+                break
 
-        amznExists = False
-        try:
-            if self.__webdriver.execute_script("return amznMusic.appConfig.customerId;"):
-                amznExists = True
-        except WebDriverException:
-            pass
-
-        if not amznExists:
-            # Should we allow the user to try again with different credentials?
-            waitMsg.close()
-            if self.__webdriver is not None:
-                self.__webdriver.quit()
-                self.__webdriver = None
-            authDialog.reject()
-            return None
+        if not element:
+            self.__webdriver.quit()
+            self.__webdriver = None
+            self.__authenticated = False
+            return False
 
         self.__amzn = {
             'deviceId'  :     self.__webdriver.execute_script("return amznMusic.appConfig.deviceId;"),
@@ -354,62 +254,8 @@ class SourceModule(modules.SourceModule):
         if not api_check or not api_check.status_code == 200:
             self.__possibly_outdated("Test request to %s failed." % self.__name, None)
             self.__authenticated = False
-            waitMsg.close()
-            authDialog.reject()
-            return None
-
-        waitMsg.close()
-        authDialog.accept()
-
-    def __reject_auth(self, authDialog):
-        authDialog.reject()
-        if self.__webdriver is not None:
-            self.__webdriver.quit()
-            self.__webdriver = None
-
-    def __track_metadata(self, d):
-        track = {}
-        track["disc"]     = d["discNum"]      if "discNum"      in d else ""
-        track["track"]    = d["trackNum"]     if "trackNum"     in d else ""
-        track["artist"]   = d["artistName"]   if "artistName"   in d and d["artistName"] != "Unknown Artist" else ""
-        track["title"]    = d["title"]        if "title"        in d else ""
-        track["duration"] = d["duration"]     if "duration"     in d else ""
-        track["album"]    = d["albumName"]    if "albumName"    in d and d["albumName"] != "Unknown Album" else ""
-        track["genre"]    = d["primaryGenre"] if "primaryGenre" in d and d["primaryGenre"] != "Unknown Genre" else ""
-        return track
-
-    def authenticate(self, window, force=False):
-        if self.__authenticated and not force:
-            return True
-        authDialog = QDialog(window)
-        authLayout = QVBoxLayout(authDialog)
-
-        authLabel = QLabel("Please authenticate with your %s account" % self.__name, authDialog)
-
-        authLayout.addWidget(authLabel)
-
-        userpassLayout = QGridLayout()
-
-        userpassLayout.addWidget(QLabel("E-mail:", authDialog), 0, 0)
-        userInput = QLineEdit(authDialog)
-        userpassLayout.addWidget(userInput, 0, 1)
-
-        userpassLayout.addWidget(QLabel("Password:", authDialog), 1, 0)
-        passInput = QLineEdit(authDialog)
-        passInput.setEchoMode(QLineEdit.Password)
-        userpassLayout.addWidget(passInput, 1, 1)
-
-        authLayout.addLayout(userpassLayout)
-
-        buttonBox = QDialogButtonBox(authDialog);
-        buttonBox.setOrientation(Qt.Horizontal)
-        buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok);
-
-        authLayout.addWidget(buttonBox);
-
-        buttonBox.accepted.connect(lambda: self.__login(authDialog, userInput.text(), passInput.text()))
-        buttonBox.rejected.connect(lambda: self.__reject_auth(authDialog))
-        return True if authDialog.exec() == QDialog.Accepted else False
+            return False
+        return True
 
     def getPlaylists(self):
         playlists_request = self.__request("cloudplayer/playlists/", "com.amazon.musicplaylist.model.MusicPlaylistService.getOwnedPlaylistsInLibrary")
